ced35106d5d11195c7f180dc0db388795fdfd4dc
Alba Cruz <albacruztorres@gmail.com>
2019-06-18 16:11:22 +0100

    js mio

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 5000;
+function helloWorld(a, b) {
+  console.log(a + b);
+  return a + b;
+}
 
-app.get("/", function(req, res) {
-  res.send(
-    '<iframe src="https://giphy.com/embed/Efpfk11XWXwTSKqgV2" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p>'
-  );
-});
-
-app.listen(PORT, function() {
-  console.log("Server is running, port " + PORT);
-});
+module.exports = {
+  helloWorld
+};
