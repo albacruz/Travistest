7c609c3ffff85a8350056f9a95678c711fac3df9
Alba Cruz <albacruztorres@gmail.com>
2019-06-18 17:14:17 +0100

    Hola

--- dist/js.d818e0ef.js
+++ dist/js.d818e0ef.js
@@ -117,9 +117,15 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"index.js":[function(require,module,exports) {
-var texto = document.getElementById("Idiota");
-texto.textContent = "Aarón es muy idiota";
+})({"src/js/index.js":[function(require,module,exports) {
+function helloWorld(a, b) {
+  console.log(a + b);
+}
+
+helloWorld(4, 7);
+module.exports = {
+  helloWorld: helloWorld
+};
 },{}],"../../../usr/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -148,7 +154,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35767" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41033" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -323,5 +329,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../usr/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/index.js.map
+},{}]},{},["../../../usr/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","src/js/index.js"], null)
+//# sourceMappingURL=/js.d818e0ef.js.map
---
+++ index.js
@@ -0,0 +1,11 @@
+function helloWorld(a, b) {
+  console.log(a + b);
+  return a + b;
+}
+
+console.log("hola");
+document.getElementById("Idiota").textContent = "Aarón es muy idiota";
+
+module.exports = {
+  helloWorld
+};
